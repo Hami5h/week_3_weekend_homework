3c9d8e8f48608c38fa4f14ff0ca6381d185eec7a
Hamish <hamishfpso@gmail.com>
2017-10-13 14:35:33 +0100

    Show which films a customer has booked to see

--- models/customer.rb
+++ models/customer.rb
@@ -49,4 +49,15 @@ class Customer
     SqlRunner.run(sql, values)
   end
 
+  def films()
+    sql = "SELECT films.* FROM films
+    INNER JOIN tickets
+    ON films.id = tickets.film_id
+    WHERE tickets.customer_id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+    film_array = results.map {|film| Film.new(film)}
+    return film_array
+  end
+
 end
