b8df129bed8f6d4972ff735fca2ca0ae992f86b5
Hamish <hamishfpso@gmail.com>
2017-10-13 12:47:58 +0100

    Customer, Films & Tickets saves passed) git remote add origin git@github.com:Hami5h/week_3_weekend_homework.git git push -u origin master \q quit

---
+++ console.rb
@@ -0,0 +1,55 @@
+require('pry')
+require_relative('models/customer.rb')
+require_relative('models/film.rb')
+require_relative('models/ticket.rb')
+
+
+
+customer_1 = Customer.new(
+  {
+    'name' => 'Daniel',
+    'funds' => '25'
+  }
+)
+customer_1.save
+
+customer_2 = Customer.new(
+  {
+    'name' => 'Callum',
+    'funds' => '30'
+  }
+)
+customer_2.save
+
+film_1 = Film.new(
+  {
+    'title' => 'spider',
+    'price' => '17'
+  }
+)
+film_1.save
+
+film_2 = Film.new(
+  {
+    'title' => 'moon',
+    'price' => '19'
+  }
+)
+film_2.save
+
+ticket_1 = Ticket.new(
+  {
+    'customer_id' => customer_1.id,
+    'film_id' => film_1.id
+  }
+)
+
+ticket_2 = Ticket.new(
+  {
+    'customer_id' => customer_2.id,
+    'film_id' => film_2.id
+  }
+)
+
+binding.pry
+nil
---
+++ models/customer.rb
@@ -0,0 +1,23 @@
+require_relative('../db/sql_runner.rb')
+require_relative('film.rb')
+require_relative('ticket.rb')
+
+class Customer
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds)
+    VALUES ($1, $2) RETURNING *"
+    values = [@name, @funds]
+    customer = SqlRunner.run(sql, values)
+    @id = customer[0]['id'].to_i()
+  end
+
+end
